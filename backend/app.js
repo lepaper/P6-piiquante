2519de61738db4519505691f427fd7d7650686ff
Dominique De Doncker <dominiquededoncker0@gmail.com>
2022-06-25 08:44:40 +0200

    modification sauce

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-
+/*const bodyParser = require('body-parser');*/
+const helmet = require('helmet');
 const app = express();
+require('dotenv');
 
 const mongoose = require('mongoose');
 const path = require('path');
-const stuffRoutes = require('./routes/stuff');
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://domi3:domi3do2607@cluster0.d7n5y.mongodb.net/?retryWrites=true&w=majority',
-  { useNewUrlParser: true,
-    useUnifiedTopology: true })
+  { useNewUrlParser: true, useUnifiedTopology: true })mong
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-app.use(express.json());
-
+app.use(helmet());
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -23,9 +22,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(bodyParser.json());
+/*app.use(bodyParser.json());*/
+app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
-
 app.use('/api/stuff', stuffRoutes);
 app.use('/api/auth', userRoutes);
 
