ea887de9b711db91ea82a33f55e87ec535e2d117
Dominique De Doncker <dominiquededoncker0@gmail.com>
2022-06-29 09:31:29 +0200

    modification sauce

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -17,11 +17,9 @@ exports.createSauce = (req, res, next) => {
 
 
 exports.getOneSauce = (req, res, next) => {
-  Sauce.findOne({
-    _id: req.params.id
-  }).then(sauce => res.status(200).json(sauce))
-    
-  .catch(error => res.status(404).json({error}));
+  Sauce.findOne({_id: req.params.id})
+   .then(sauce => res.status(200).json(sauce))
+   .catch(error => res.status(404).json({error}));
     
 }
 
