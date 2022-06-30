f0bf25da38bb930b99f30193f028225a65bbf768
Dominique De Doncker <dominiquededoncker0@gmail.com>
2022-06-30 07:58:14 +0200

    ajout des likes

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -6,7 +6,7 @@ exports.signup = (req, res, next) => {
     bcrypt.hash(req.body.password, 10)
       .then(hash => {
         const user = new User({
-          email: req.body.email,
+          email: hashedEmail,
           password: hash
         });
         user.save()
@@ -16,19 +16,47 @@ exports.signup = (req, res, next) => {
       .catch(error => res.status(500).json({ error }));
   };
 
+  /*exports.login = (req, res, next) => {
+    User.findOne({ email: req.body.email })
+        .then(user => {
+            // Vérification de l'email utilisateur
+            if(!user) {
+                return res.status(401).json({ error: 'Utilisateur inconnu'});
+            } else {
+            // Vérification du mot de passe
+            bcrypt.compare(req.body.password, user.password)
+                .then(valid => {
+                    if(!valid) {
+                        return res.status(401).json({ error: 'Mot de passe incorrect'});
+                    } else {
+                        // Si ok validation de la connexion et attribution d'un token d'authentifcation
+                        let token = jwt.sign(
+                            {userId: user._id},
+                            process.env.SECRET_TOKEN,
+                            { expiresIn: '24h' }                        
+                        );
+                        res.status(200).json({
+                            userId: user._id,
+                            token: token
+                        });
+                    }
+                })*/
+
   exports.login = (req, res, next) => {
     User.findOne({ email: req.body.email })
       .then(user => {
         if (!user) {
           return res.status(401).json({ error: 'Utilisateur non trouvé !' });
         }
+        else {
 
         bcrypt.compare(req.body.password, user.password)
           .then(valid => {
             if (!valid) {
               return res.status(401).json({ error: 'Mot de passe incorrect !' });
             }
-            /*console.table(req.body);*/
+            else {
+            console.table(req.body);
             res.status(200).json({
               userId: user._id,
               token: jwt.sign(
@@ -36,9 +64,12 @@ exports.signup = (req, res, next) => {
                 'RANDOM_TOKEN_SECRET',
                 { expireIn: '24h'}
               )
+            
             });
+          }
           })
           .catch(error => res.status(500).json({ error }));
+        }
       })
       .catch(error => res.status(500).json({ error }));
   };
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -14,6 +14,6 @@ module.exports = (req, res, next) => {
   } catch {
     res.status(401).json({
       error: new Error('Invalid request!')
-    });
+    })
   }
 };
